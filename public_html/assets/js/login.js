fdd4af12ed2e2ea464b0f9eaa53c8bf0303ca8d8
David Wibergh <david@wibergh.com>
2015-05-25 12:59:34 +0200

    Bug improvement

--- public_html/assets/js/login.js
+++ public_html/assets/js/login.js
@@ -9,34 +9,35 @@ $(function () {
     $("#login").submit(function(event) {
         event.preventDefault();
 
-        console.log('login');
+        console.log('login1');
 
         // Hide error message
         var error = $(".error"), username = $("#username"), password = $("#password"), button = $("#login").find('button');
         error.addClass('hidden');
 
         //displayMessage('info', 'Inloggning pågår', 'OVPNbox arbetar på att verifiera inloggningsuppgifterna & ändra inställningar i boxen.');
-
+        console.log('login2');
         // Update button
         button.html('<i class="fa fa-circle-o-notch fa-spin"></i> Verifierar');
+        console.log('login3');
 
         $.ajax({
             type: "POST",
             url:  "/api/authenticate",
             data: {
-                username: username,
-                password: password
+                username: username.val(),
+                password: password.val()
             },
             async: true,
             cache: false,
-            timeout:60000,
+            timeout:120000,
             success: function () {
-
+                console.log('login4');
                 window.location = '/';
 
             },
             error: function(xhr, textStatus, errorThrown ) {
-
+                console.log('login');
                 try {
                     var err = JSON.parse(xhr.responseText);
                 } catch(error) {
@@ -47,6 +48,7 @@ $(function () {
                 button.html('Logga in');
             }
         });
+        console.log('login5');
     });
 
 });
